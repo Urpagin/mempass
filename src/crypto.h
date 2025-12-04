9e892e28b51c3a42f65183aaefd505c95a8e98ef
Urpagin <urpagin@proton.me>
2025-12-04 21:23:32 +0100

    Fix shred; fix print

---
+++ src/crypto.h
@@ -0,0 +1,86 @@
+/*
+ * Author: Urpagin
+ * Date: 2025-12-01
+ * Description: crypto.h, functions related to hashing, memory and security.
+ * License: Copyright (C) 2025 Urpagin (GPL v2)
+ * File: crypto.h
+ */
+
+#pragma once
+
+
+#include "str.h"
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// Shreds a memory zone up to and including `size`
+// bytes, `passes` times over using random data
+// from /dev/random.
+// Returns 0 on success, -1 on error.
+// if size == 0, or passes == 0, returns 0 not performing anything.
+static int shred(void *data, const size_t size,
+                          const size_t passes) {
+
+    // I know it's useless because of the forloop (passes), but who cares!
+    // No one's ever going to read this, anyway.
+    if (size == 0 || passes == 0 || !data) {
+        return 0;
+    }
+
+    // To middle-finger the compiler from possibly
+    // optimizing away our pretty logic.
+    volatile uint8_t *p = (volatile uint8_t*)data;
+
+    int fd = open("/dev/random", O_RDONLY);
+    if (fd == -1) {
+        perror("Failed to open /dev/random");
+        return -1;
+    }
+
+    const size_t RND_SIZE = 64;
+    uint8_t rnd[RND_SIZE];
+
+    for (size_t pass = 0; pass < passes; ++pass) {
+        // Where we are in the p.
+        size_t idx = 0;
+        ssize_t r;
+        while (idx < size && (r = read(fd, rnd, RND_SIZE)) > 0) {
+            // Loop on r, because read() can return any 1 <= r <= RND_SIZE
+            for (size_t i = 0; i < (size_t)r; ++i) {
+                // Shred one byte at a time.
+                p[idx++] = rnd[i];
+            }
+        }
+
+        if (r == -1) {
+            perror("Error reading random data");
+            close(fd);
+            return -1;
+        }
+        // This should never happen.
+        if (r == 0) {
+            perror("EOF reached on /dev/random???");
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+    return 0;
+}
+
+// Shreds the pointer a hundred times and frees it.
+// size is the size of the allocated memory.
+static inline int obliterate_str(str *s) {
+    if (!s->cap || !s->len) {
+        free(s->buf);
+        return 0;
+    }
+
+    int ret = shred(s->buf, s->cap, 100);
+    free(s->buf);
+    return ret;
+}
+
--- src/str.h
+++ src/str.h
@@ -3,7 +3,10 @@
  * Date: 2025-12-01
  * Description: A small header-only dynamic string implementation.
  * License: Copyright (C) 2025 Urpagin (GPL v2)
+ * File: str.h
  */
+
+#pragma once
 
 // For getline()
 #define _POSIX_C_SOURCE 200809L
@@ -17,90 +20,89 @@
 
 // A custom String.
 typedef struct String {
-  size_t cap;
-  size_t len;
-  char *buf;
+    size_t cap;
+    size_t len;
+    char *buf;
 } str;
 
 // Instantiates a new String with
 // capacity is the initial capacity allocated.
 static inline str str_new(size_t capacity) {
-  if (!capacity) {
-    // "compound literal" syntax.
-    return (str){0, 0, NULL};
-  }
+    if (!capacity) {
+        // "compound literal" syntax.
+        return (str){0, 0, NULL};
+    }
 
-  return (str){capacity, 0, (char *)malloc(capacity)};
+    return (str){capacity, 0, (char *)malloc(capacity)};
 }
 
 // Appends a buffer to a str.
 static inline void str_append_buf(str *s, const char buf[], const size_t size) {
-  if (s->cap < (s->len + size)) {
-    size_t growth = (s->cap + size) * 1.5;
-    void *p = realloc(s->buf, growth);
-    if (!p) {
-      perror("Failed to reallocate");
-      exit(1);
+    if (s->cap < (s->len + size)) {
+        size_t growth = (s->cap + size) * 1.5;
+        void *p = realloc(s->buf, growth);
+        if (!p) {
+            perror("Failed to reallocate");
+            exit(1);
+        }
+        s->buf = (char *)p;
+        // s->cap += growth;
+        s->cap = growth;
     }
-    s->buf = (char *)p;
-    // s->cap += growth;
-    s->cap = growth;
-  }
 
-  // +1: null byte
-  memcpy((s->buf + s->len), buf, size);
-  s->len += size;
+    // +1: null byte
+    memcpy((s->buf + s->len), buf, size);
+    s->len += size;
 }
 
 // Appends a char* to a str.
 static inline void str_append(str *s, const char *buf) {
-  str_append_buf(s, buf, strlen(buf));
+    str_append_buf(s, buf, strlen(buf));
 }
 
 // Clears the str and deallocates it.
 // Effectively rendering it the same as the return value of str_new().
 static inline void str_clear(str *s) {
-  free(s->buf);
-  s->cap = 0;
-  s->len = 0;
+    free(s->buf);
+    s->cap = 0;
+    s->len = 0;
 }
 
 // Prints a str.
 // Adds a newline.
 static inline void str_print(const str *s) {
-  if (!s->len)
-    return;
+    if (!s->len)
+        return;
 
-  fwrite(s->buf, 1, s->len, stdout);
-  fwrite("\n", 1, 2, stdout);
+    fwrite(s->buf, sizeof(char), s->len, stdout);
+    printf("\n");
 }
 
 // Prints the `prompt` argument to stdout without any newline.
 // Reads stdin using getline().
 // Returns a str.
-static inline str str_from_stdin(char *prompt) {
-  size_t init_cap = 32;
+static inline str str_from_stdin(const char *prompt) {
+    size_t init_cap = 32;
 
-  str s = str_new(init_cap);
+    str s = str_new(init_cap);
 
-  printf("%s", prompt);
+    printf("%s", prompt);
 
-  ssize_t read = getline(&s.buf, &init_cap, stdin);
-  if (read == -1) {
-    perror("Failed to read user input");
+    ssize_t read = getline(&s.buf, &init_cap, stdin);
+    if (read == -1) {
+        perror("Failed to read user input");
+        return s;
+    }
+
+    s.cap = init_cap;
+    s.len = read;
+
     return s;
-  }
-
-  s.cap = init_cap;
-  s.len = read;
-
-  return s;
 }
 
 static inline void str_debug(const str *s) {
-  printf("String: {capacity: %ld; length: %ld}\n", s->cap, s->len);
+    printf("String: {capacity: %ld; length: %ld}\n", s->cap, s->len);
 }
-
 
 // Sets the ECHO bit of the terminal file fd to:
 // OFF if state == 0
@@ -108,19 +110,55 @@ static inline void str_debug(const str *s) {
 // TODO: return int, 0 success -1 error.
 //
 // TODO: After pressing [ENTER] after getting the user input,
-// TODO: it echoes the full text at the end. Altough the ECHO works WHILE the user it typing.
+// TODO: it echoes the full text at the end. Altough the ECHO works WHILE the
+// user it typing.
 static inline void set_echo(const int fd, int state) {
     struct termios t;
     // Gets the existing termios config for the fd.
     tcgetattr(fd, &t);
 
     // ON
-    if (state) t.c_lflag |= ECHO;
+    if (state)
+        t.c_lflag |= ECHO;
     // OFF
-    else t.c_lflag &= ~ECHO;
+    else
+        t.c_lflag &= ~ECHO;
 
     // Apply the changes
     tcsetattr(fd, TCSANOW, &t);
 }
 
 
+
+// Removes the last \10 (\n) if there is one
+// and replaces it with a null terminator.
+//
+// If the string's len is 0, no-op.
+static inline void trim_last(str *s) {
+    if (!s->len) return ;
+
+    // Last char
+    const size_t last_idx = s->len - 1;
+
+    if (s->buf[last_idx] == '\n') {
+        s->buf[last_idx] = '\0';
+    }
+}
+
+
+// Get password securely
+// Trims any newlines.
+static inline str get_pass(void) {
+    // Disable ECHO
+    set_echo(STDIN_FILENO, 0);
+
+    str pass = str_from_stdin("Secret: ");
+
+    trim_last(&pass);
+
+    // Revert back to normal.
+    set_echo(STDIN_FILENO, 1);
+
+
+    return pass;
+}
