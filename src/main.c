aa831e5a5cef524a1a43f78c99aef7eebb8da0a7
Urpagin <urpagin@proton.me>
2025-12-04 12:07:51 +0100

    Add simple stdin reading

--- src/main.c
+++ src/main.c
@@ -1,36 +1,17 @@
 /*
-* Author: Urpagin
-* Date: 2025-12-01
-* Description: mempass, a simple utility to memorize a secret.
-* License: Copyright (C) 2025 Urpagin (GPL v2)
-*/
+ * Author: Urpagin
+ * Date: 2025-12-01
+ * Description: mempass, a simple utility to memorize a secret.
+ * License: Copyright (C) 2025 Urpagin (GPL v2)
+ */
 
 #include "str.h"
-#include <stdio.h>
-
-
-// Prompts the user for an input.
-str get_usr_input(char *prompt) {
-    char buf[32];
-    str s = str_new();
-    printf("%s", prompt);
-    for (;;) {
-        char *r = fgets(buf, sizeof(buf), stdin);
-        printf("Lap\n");
-        str_append(&s, buf);
-        if (!r) {
-            break;
-        }
-    }
-    printf("\n");
-    return s;
-}
 
 int main(int argc, char *argv[]) {
+    str u = str_from_stdin("Usr: ");
 
-    str u = get_usr_input("Prompt: ");
-    str_print(&u);
+  // str u = get_usr_input("Prompt: ");
+  str_print(&u);
 
-    return 0;
+  return 0;
 }
-
--- src/str.h
+++ src/str.h
@@ -1,9 +1,12 @@
 /*
-* Author: Urpagin
-* Date: 2025-12-01
-* Description: A small header-only dynamic string implementation.
-* License: Copyright (C) 2025 Urpagin (GPL v2)
-*/
+ * Author: Urpagin
+ * Date: 2025-12-01
+ * Description: A small header-only dynamic string implementation.
+ * License: Copyright (C) 2025 Urpagin (GPL v2)
+ */
+
+// For getline()
+#define _POSIX_C_SOURCE 200809L
 
 #include <stddef.h>
 #include <stdio.h>
@@ -12,79 +15,86 @@
 
 // A custom String.
 typedef struct String {
-    size_t cap;
-    size_t len;
-    char* buf;
+  size_t cap;
+  size_t len;
+  char *buf;
 } str;
 
-
-// Instantiates a new String with 
-static inline str str_new(void) {
+// Instantiates a new String with
+// capacity is the initial capacity allocated.
+static inline str str_new(size_t capacity) {
+  if (!capacity) {
     // "compound literal" syntax.
     return (str){0, 0, NULL};
+  }
+
+  return (str){capacity, 0, (char *)malloc(capacity)};
 }
 
 // Appends a buffer to a str.
 static inline void str_append_buf(str *s, const char buf[], const size_t size) {
-    if (s->cap < (s->len + size)) {
-        size_t growth = (s->cap + size) * 1.5;
-        void *p = realloc(s->buf, growth);
-        if (!p) {
-            perror("Failed to reallocate");
-            exit(1);
-        }
-        s->buf = (char*)p;
-        //s->cap += growth;
-        s->cap = growth;
+  if (s->cap < (s->len + size)) {
+    size_t growth = (s->cap + size) * 1.5;
+    void *p = realloc(s->buf, growth);
+    if (!p) {
+      perror("Failed to reallocate");
+      exit(1);
     }
+    s->buf = (char *)p;
+    // s->cap += growth;
+    s->cap = growth;
+  }
 
-    // +1: null byte
-    memcpy((s->buf + s->len), buf, size);
-    s->len += size;
+  // +1: null byte
+  memcpy((s->buf + s->len), buf, size);
+  s->len += size;
 }
 
 // Appends a char* to a str.
 static inline void str_append(str *s, const char *buf) {
-    str_append_buf(s, buf, strlen(buf));
+  str_append_buf(s, buf, strlen(buf));
 }
 
 // Clears the str and deallocates it.
 // Effectively rendering it the same as the return value of str_new().
 static inline void str_clear(str *s) {
-    free(s->buf);
-    s->cap = 0;
-    s->len = 0;
+  free(s->buf);
+  s->cap = 0;
+  s->len = 0;
 }
 
 // Prints a str.
 // Adds a newline.
 static inline void str_print(const str *s) {
-    if (!s->len) 
-        return;
+  if (!s->len)
+    return;
 
-    fwrite(s->buf, 1, s->len, stdout);
-    fwrite("\n", 1, 2, stdout);
+  fwrite(s->buf, 1, s->len, stdout);
+  fwrite("\n", 1, 2, stdout);
 }
 
-// Returns whether a str contains a substring.
-static inline bool str_contains(const str *s, const char *p) {
-    // loop str
-    for (size_t i = 0; i < s->len; ++i) {
-        // if first char of pattern in str.
-        if (s->buf[i] == p[0]) {
-            // loop pattern
-            bool is_matching = false;
-            for (size_t j = 0; j < strlen(p); ++j) {
-                if (s->buf[l])
+// Prints the `prompt` argument to stdout without any newline.
+// Reads stdin using getline().
+// Returns a str.
+static inline str str_from_stdin(char *prompt) {
+  size_t init_cap = 32;
 
-                if ()
-            }
-        }
-    }
+  str s = str_new(init_cap);
+
+  printf("%s", prompt);
+
+  ssize_t read = getline(&s.buf, &init_cap, stdin);
+  if (read == -1) {
+    perror("Failed to read user input");
+    return s;
+  }
+
+  s.cap = init_cap;
+  s.len = read;
+
+  return s;
 }
 
 static inline void str_debug(const str *s) {
-    printf("String: {capacity: %ld; length: %ld}\n", s->cap, s->len);
+  printf("String: {capacity: %ld; length: %ld}\n", s->cap, s->len);
 }
-
-
