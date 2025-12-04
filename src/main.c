0e785daba5449f058ace697c5211d1a1ffd762ab
Urpagin <urpagin@proton.me>
2025-12-04 22:43:24 +0100

    Add todolist

--- src/main.c
+++ src/main.c
@@ -11,6 +11,23 @@
 #include <stdint.h>
 #include <unistd.h>
 
+// TODO: (THICC) all that is below:
+
+/*
+* Usage:
+* ./mempass (interactive, asks for one secret and then it's test time.)
+*
+* ./mempass [--generate|-g] (prompts for one or more secrets and also add a label) (JSON or custom format)
+* TODO: requires ncurses?
+*
+* ./mempass [FILE_PATH] (then screen selector for which secret would the user like to train on)
+*
+* ./mempass [FILE_PATH] [--random|-r] (prompts the user with a random label for them to train on)
+*
+* ./mempass [FILE_PATH] [--label|-l] (directly enters training mode.)
+*
+*/
+
 int main(int argc, char *argv[]) {
 
     str s = get_pass();
